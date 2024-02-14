a8405810e745ae0617e1a0a937684317c973a407
Kiran19122001 <kiranban0011@gmail.com>
2024-02-14 22:00:06 +0530

    chat app

--- server/index.js
+++ server/index.js
@@ -3,16 +3,18 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
-const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true,
-  optionSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 const server = http.createServer(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+    optionSuccessStatus: 200,
+  },
+});
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
